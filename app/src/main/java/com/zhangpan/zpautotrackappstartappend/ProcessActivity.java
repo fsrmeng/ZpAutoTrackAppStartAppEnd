e69d90724b9ee40c2879d84498e3604293c7f808
panzhang <Azp1993213201788>
2019-07-08 16:49:54 +0800

    Initial commit

---
+++ app/src/main/java/com/zhangpan/zpautotrackappstartappend/ProcessActivity.java
@@ -0,0 +1,13 @@
+package com.zhangpan.zpautotrackappstartappend;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ProcessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_process);
+    }
+}
